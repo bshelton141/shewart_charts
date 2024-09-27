fa68c63913052b3e1a7758a767fd12fda992eaf6
bshelton141 <brandon.michael.shelton@gmail.com>
2024-09-27 06:50:33 -0700

    Update data_loads.py

--- shewhart/data_loads.py
+++ shewhart/data_loads.py
@@ -2,63 +2,61 @@ import pkg_resources
 import pandas as pd
 
 
-def enc_submits_monthly():
-    """Return a dataframe that provides monthly total encounters volumes.
+def claim_submits_monthly():
+    """Return a synthetic dataframe that provides monthly total encounters volumes.
     
     Contains the following fields:
         rcvd_month      68 non-null object
-        enc_count       68 non-null object
-        val             68 non-null object
+        claim_volume       68 non-null object
     ... (docstring truncated) ...
     """
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/enc_submits_monthly.csv')
+    stream = pkg_resources.resource_stream(__name__, 'data/claim_submits_monthly.csv')
     return pd.read_csv(stream)
 
 
-def enc_by_formtype_monthly():
-    """Return a dataframe that provides monthly utilization count by medical encounter type.
+def claim_submits_monthly_by_formtype():
+    """Return a synthetic dataframe that provides monthly utilization count by medical encounter type.
     
     Contains the following fields:
         rcvd_month      68 non-null object
-        file_fmt_cd     68 non-null object
-        enc_count       68 non-null object
+        formtype     68 non-null object
+        claim_volume       68 non-null object
     ... (docstring truncated) ...
     """
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/enc_by_formtype_monthly.csv')
+    stream = pkg_resources.resource_stream(__name__, 'data/claim_submits_monthly_by_formtype.csv')
     return pd.read_csv(stream)
 
 
-def enc_reject_rate_monthly():
-    """Return a dataframe that provides monthly utilization rejected encounters, total enounters,
+def claim_reject_rate_monthly():
+    """Return a synthetic dataframe that provides monthly utilization rejected encounters, total enounters,
        and rejection rate.
     
     Contains the following fields:
-        month_elig      68 non-null object
+        service_month      68 non-null object
         reject_count    68 non-null object
         total_count     68 non-null object
-        rej_rate        68 non-null object
+        reject_rate        68 non-null object
     ... (docstring truncated) ...
     """
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/enc_reject_rate_monthly.csv')
+    stream = pkg_resources.resource_stream(__name__, 'data/claim_reject_rate_monthly.csv')
     return pd.read_csv(stream)
 
 
-def tran_office_rej_by_ppg():
-    """Return a dataframe for a 6-month period of Jul 2023 thru Dec 2023 of internally-rejected 
-       office place of service encounters ('10', '11', '49', '72') from Transunion submissions,
-       by PPG.
+def claim_reject_rate_by_clinic():
+    """Return a synthetic dataframe for a 6-month period of rejected utilization submissions,
+       by synthetic provider group.
     
     Contains the following fields:
-        ppg_code        68 non-null object
+        clinic        68 non-null object
         reject_count    68 non-null object
         total_count     68 non-null object
-        rej_rate        68 non-null object
+        reject_rate        68 non-null object
     ... (docstring truncated) ...
     """
     # This is a stream-like object. If you want the actual info, call
@@ -67,36 +65,36 @@ def tran_office_rej_by_ppg():
     return pd.read_csv(stream)
 
 
-def hdr_bp_cd_status_rej_rate():
-    """Return a dataframe that provides monthly utilization rejected encounters, total enounters,
+def claim_reject_rate_monthly_by_submitter():
+    """Return a synthetic dataframe that provides monthly utilization rejected encounters, total enounters,
        and rejection rate by encounter submission entity.
     
     Contains the following fields:
-        month_elig      68 non-null object
-        hdr_bp_cd       68 non-null object
+        service_month      68 non-null object
+        submitter       68 non-null object
         reject_count    68 non-null object
         total_count     68 non-null object
-        rej_rate        68 non-null object
+        reject_rate        68 non-null object
     ... (docstring truncated) ...
     """
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/hdr_bp_cd_status_rej_rate.csv')
+    stream = pkg_resources.resource_stream(__name__, 'data/claim_reject_rate_monthly_by_submitter.csv')
     return pd.read_csv(stream)
 
 
-def pcp_pmpm_data():
-    """Return a dataframe that provides monthly PCP utilization and member month counts, as well
+def util_pmpm():
+    """Return a synthetic dataframe that provides monthly utilization and member month counts, as well
        as the calculated per member per month (PMPM) measure.
     
     Contains the following fields:
-        month_elig      68 non-null object
-        pcp_count       68 non-null object
+        service_month     68 non-null object
+        util_count       68 non-null object
         mem_count       68 non-null object
-        pcp_pmpm        68 non-null object
+        pmpm        68 non-null object
     ... (docstring truncated) ...
     """
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/pcp_pmpm_data.csv')
+    stream = pkg_resources.resource_stream(__name__, 'data/util_pmpm.csv')
     return pd.read_csv(stream)
